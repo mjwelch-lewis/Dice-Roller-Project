e2f45c58c7ff9c5753c22ae8cc8e9c306d0f295e
mjwelch-lewis <matthewjwelch@lewisu.edu>
2022-09-28 20:24:15 -0500

    Added comments to script file

--- static/index.js
+++ static/index.js
@@ -1,3 +1,4 @@
+//calculates a random number between 1 and 6 and updates the html element value
 function RollDice() {
     document.getElementById("dice").value = Math.floor(Math.random() * 6) + 1;
 }
